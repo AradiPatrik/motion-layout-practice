6299ea98fdfb34628b5df49081ba83c0468b4b36
Patrik Aradi <patrik.aradi@supercharge.io>
2020-10-14 08:57:11 +0200

    add phone icon overshoot animation

--- app/src/main/java/com/example/motionlayoutpractice/MainActivity.kt
+++ app/src/main/java/com/example/motionlayoutpractice/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.ListAdapter
